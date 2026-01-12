f8ef2329c0f95ca36ba3faef4a833e260ca07aa9
Om Pawar <om.d.pawar.214@gmail.com>
2026-01-12 14:28:46 +0530

    Added feature to add the new category

--- src/main/java/com/ecommerce/Ecommerce_App/controller/CategoryController.java
+++ src/main/java/com/ecommerce/Ecommerce_App/controller/CategoryController.java
@@ -1,19 +1,29 @@
 package com.ecommerce.Ecommerce_App.controller;
 
 import com.ecommerce.Ecommerce_App.Model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
 
+    //dummy list to initially store teh categories
+    List<Category> categories = new ArrayList<>();
+
     // method to fetch all categories
     @GetMapping("/public/categories")
     public List<Category> getAllCategories(){
-        return CategoryService.getAllCategories();
+        return categories;
     }
+
+    //method to store teh category
+    @PostMapping("/admin/category")
+    public String addCategory(@RequestBody Category category){
+        categories.add(category);
+         return "Category Added Successfully !!";
+    }
+
 }
