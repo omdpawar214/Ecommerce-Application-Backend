1bff779708f156c0ef51fcbde54908bb0ef30994
Om Pawar <om.d.pawar.214@gmail.com>
2026-01-24 20:13:42 +0530

    Added Product Entity Model

---
+++ src/main/java/com/ecommerce/Ecommerce_App/Model/Product.java
@@ -0,0 +1,34 @@
+package com.ecommerce.Ecommerce_App.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long productId;
+    @NotBlank
+    private String productName;
+    @NotBlank
+    private String productDescription;
+    @NotNull
+    private Integer price;
+    @NotNull
+    private Integer specialPrice;
+    @NotNull
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category_Id")
+    private Category category;
+}
