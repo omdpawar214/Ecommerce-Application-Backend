7517cb612221171f0ba607b73ca66df9e67523bc
Om Pawar <om.d.pawar.214@gmail.com>
2026-02-17 20:19:57 +0530

    Provided custom userDetailsService implementation to spring boot , to make use of our own userDetails class and own User Entity

---
+++ src/main/java/com/ecommerce/Ecommerce_App/securityRequirements/userDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ecommerce.Ecommerce_App.securityRequirements;
+
+import com.ecommerce.Ecommerce_App.Model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class userDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(()->new UsernameNotFoundException("User name not found :" + username));
+
+        return userDetailsImpl.build(user);
+    }
+}
