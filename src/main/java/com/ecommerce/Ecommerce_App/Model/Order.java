11c1b2a23253b9b674e95166ca59e2fdebb02c1f
Om Pawar <om.d.pawar.214@gmail.com>
2026-03-02 20:38:25 +0530

    Added Model of Order which will help to manage the orders of users and orderItems

---
+++ src/main/java/com/ecommerce/Ecommerce_App/Model/Order.java
@@ -0,0 +1,39 @@
+package com.ecommerce.Ecommerce_App.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.invoke.CallSite;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+    @Email
+    private String email;
+    private String orderStatus;
+    private LocalDate orderDate;
+    private Double totalAmount;
+
+    @OneToOne
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
+
+    @OneToMany(mappedBy = "order ",cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "order_address_Id")
+    private Address orderAddress;
+}
